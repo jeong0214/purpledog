ab03088b5302142bf3d9c2b1452f0601dce961e9
jeong0214 <wjdtjr0214@gmail.com>
2023-02-22 13:07:30 +0900

    first commit

--- matching.js
+++ matching.js
@@ -5,6 +5,22 @@ const next = document.getElementById("btn-next");
 const next1 = document.getElementById("btn-next2");
 const step1_modal = document.getElementById("step1_modal");
 const step2_modal = document.getElementById("step2_modal");
+const count = document.querySelectorAll(".bottle_count");
+const recommend = document.querySelectorAll(".btn-recommend");
+console.log(count);
+console.log(recommend);
+
+recommend.forEach(function (step2) {
+  step2.addEventListener("click", function () {
+    next1.style.backgroundColor = "#aeaeae";
+  });
+});
+
+count.forEach(function (step) {
+  step.addEventListener("click", function () {
+    next.style.backgroundColor = "#aeaeae";
+  });
+});
 
 grade.forEach(function (item, i, test) {
   item.addEventListener("click", function () {
